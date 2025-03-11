698a90f380f7b0aaf55952689c2e63ecfbe97a05
Guilherme Freitas do Nascimento <121752740+SrFreitass@users.noreply.github.com>
2025-03-11 16:21:17 -0400

    ✨ feat(front): add react router dom

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App/>} />
+    </Routes>
+  </BrowserRouter>
 )
 
 // Use contextBridge
