ad37dd4fce0e1a902dad01d96cfd2444d8023d3f
Pedro Celestino <pedrojuniorcelestinocg@gmail.com>
2025-03-27 15:57:47 -0400

    feat: componentização card

---
+++ front-end/src/components/card.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { cn } from '@/lib/utils';
+
+interface CardProps {
+    imageSrc: string;
+    imageAlt?: string;
+    title: string;
+    description?:string;
+    className?:string;
+    imageClassName?:string;
+}
+
+const Card = React.forwardRef<HTMLDivElement, CardProps>(
+    ({
+        imageSrc,
+        imageAlt = "",
+        title,
+        description,
+        className,
+        imageClassName,
+    },
+    ref
+    ) => {
+        return  (
+                <div
+                    ref={ref}
+                    className={cn('flex flex-col gap-1 bg-white p-4 rounded-2xl w-72 drop-shadow-sm',className)}
+                >
+                <img className={cn('size-6 rounded-full',imageClassName)}
+                    src={imageSrc}
+                    alt={imageAlt}
+                />
+                <div className={cn('text-left overflow-auto break-words',className)}>
+                    <h3 className="text-[1rem]">{title}</h3>
+                    <p className="text-[#5A5A65] text-[14px]" >{description}</p>
+                </div>
+                </div>
+            );
+        }
+);
+
+Card.displayName = "Card";
+
+export {Card};
